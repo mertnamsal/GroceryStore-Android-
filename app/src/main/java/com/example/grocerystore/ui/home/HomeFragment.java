1ec0c90f4195fec4d2571ed2fcd68e5fd62adae6
mertnamsal <namsalmert@gmail.com>
2024-03-05 17:56:49 +0300

    home fragment fix

--- app/src/main/java/com/example/grocerystore/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/grocerystore/ui/home/HomeFragment.java
@@ -10,13 +10,18 @@ import androidx.fragment.app.Fragment;
 
 
 import com.example.grocerystore.R;
+import com.example.grocerystore.adapters.PopularAdapters;
+import com.example.grocerystore.model.PopularModel;
+
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
 
 
     //popular items
-
+    List<PopularModel> popularModelList;
+    PopularAdapters popularAdapters;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container,false);
