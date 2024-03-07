447c5ec711ec3bbcf6a9a1a5b20ec8aa27df0949
mertnamsal <namsalmert@gmail.com>
2024-03-07 18:06:13 +0300

    design of category fragment

---
+++ app/src/main/java/com/example/grocerystore/adapters/NavCategoryAdapter.java
@@ -0,0 +1,61 @@
+package com.example.grocerystore.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.grocerystore.R;
+import com.example.grocerystore.model.NavCategoryModel;
+
+import java.util.List;
+
+public class NavCategoryAdapter extends RecyclerView.Adapter<NavCategoryAdapter.ViewHolder> {
+
+    Context context;
+    List<NavCategoryModel> navCategoryModelList;
+
+    public NavCategoryAdapter(Context context, List<NavCategoryModel> navCategoryModelList) {
+        this.context = context;
+        this.navCategoryModelList = navCategoryModelList;
+    }
+
+    @NonNull
+    @Override
+    public NavCategoryAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.nav_cat_item,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NavCategoryAdapter.ViewHolder holder, int position) {
+
+        Glide.with(context).load(navCategoryModelList.get(position).getImg_url()).into(holder.imageView);
+        holder.name.setText(navCategoryModelList.get(position).getName());
+        holder.description.setText(navCategoryModelList.get(position).getDescription());
+        holder.discount.setText(navCategoryModelList.get(position).getDiscount());
+    }
+
+    @Override
+    public int getItemCount() {
+        return navCategoryModelList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView name,description,discount;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.cat_nav_img);
+            name = itemView.findViewById(R.id.nav_cat_name);
+            description = itemView.findViewById(R.id.nav_cat_description);
+            discount = itemView.findViewById(R.id.nav_cat_discount);
+        }
+    }
+}
