1f03439fa6eb2a8b395d9bcdd7ecd64f56ab62c2
mertnamsal <namsalmert@gmail.com>
2024-03-13 18:50:56 +0300

    added my carts

--- app/src/main/java/com/example/grocerystore/MyCartsFragment.java
+++ app/src/main/java/com/example/grocerystore/MyCartsFragment.java
@@ -2,14 +2,35 @@ package com.example.grocerystore;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.grocerystore.adapters.MyCartAdapter;
+import com.example.grocerystore.model.MyCartModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyCartsFragment extends Fragment {
 
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+
+    RecyclerView recyclerView;
+    MyCartAdapter cartAdapter;
+    List<MyCartModel> cartModelList;
     public MyCartsFragment() {
         // Required empty public constructor
     }
@@ -17,6 +38,29 @@ public class MyCartsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_carts, container, false);
+        View root =  inflater.inflate(R.layout.fragment_my_carts, container, false);
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        recyclerView = root.findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        cartModelList = new ArrayList<>();
+        cartAdapter = new MyCartAdapter(getActivity(),cartModelList);
+        recyclerView.setAdapter(cartAdapter);
+                db.collection("AddToCart").document(auth.getCurrentUser().getUid())
+                        .collection("CurrentUser").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if(task.isSuccessful()){
+                                    for(DocumentSnapshot documentSnapshot : task.getResult().getDocuments()){
+                                        MyCartModel myCartModel = documentSnapshot.toObject(MyCartModel.class);
+                                        cartModelList.add(myCartModel);
+                                        cartAdapter.notifyDataSetChanged();
+                                    }
+                                }
+                            }
+                        });
+
+        return root;
     }
 }
---
+++ app/src/main/java/com/example/grocerystore/model/MyCartModel.java
@@ -0,0 +1,71 @@
+package com.example.grocerystore.model;
+
+public class MyCartModel {
+
+    private String productName;
+    private String productPrice;
+    private String currentDate;
+    private String currentTime;
+    private String totalQuantity;
+    private int totalPrice;
+
+    public MyCartModel() {
+    }
+
+    public MyCartModel(String productName, String productPrice, String currentDate, String currentTime, String totalQuantity, int totalPrice) {
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.currentDate = currentDate;
+        this.currentTime = currentTime;
+        this.totalQuantity = totalQuantity;
+        this.totalPrice = totalPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(String productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+
+    public String getTotalQuantity() {
+        return totalQuantity;
+    }
+
+    public void setTotalQuantity(String totalQuantity) {
+        this.totalQuantity = totalQuantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
